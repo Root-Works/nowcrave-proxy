7c9189519d6850f444b5a041db0ae3dba78e54fb
RootWorks <root@rootworks.co.kr>
2025-07-15 19:53:19 +0900

    fix: remove scope to avoid invalid JWT request

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ app.post("/recommend", async (req, res) => {
 
     const auth = new GoogleAuth({
       credentials: JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON),
-      scopes: "https://www.googleapis.com/auth/cloud-platform",
     });
 
     const client = await auth.getIdTokenClient(TARGET_URL);
